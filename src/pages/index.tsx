35e8609cab5b84a59e30d115c38f9d710f6bec4b
samitariq12 <samitariq40@gmail.com>
2020-12-29 00:28:43 +0500

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import BlogList from "../components/blogList"
 import Footer from "../components/footer"
 
